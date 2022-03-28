9d5cde3c56b901d728870f953012857d3f5c6f7f
batman <batman@flyup.tw>
2022-03-28 10:23:18 +0800

    update

--- src/api/index.js
+++ src/api/index.js
@@ -4,14 +4,3 @@ export const me = () => Axios.get( '/member/member/me' );
 // 用户 VIP 資訊
 export const getMemberVipInfo = () => Axios.get( '/member/vip/info' );
 
-// export const login = (payload) => Axios.post('/users/login', payload);
-// // 用户注册
-// export const register = (payload) => Axios.post('/users/reg', payload);
-// // 获取注册验证码
-// export const getRegCode = (payload) => Axios.post('/users/code', payload);
-// // 获取找回密码验证码
-// export const getForgotCode = (payload) => Axios.post('/users/forget/code', payload);
-// // 找回密码
-// export const resetPass = (payload) => Axios.post('/users/reset/pass', payload);
-// // 公告列表
-// export const noticeList = (payload) => Axios.get('/notice/list', { params: payload });
